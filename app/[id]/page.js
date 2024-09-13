b90203da5e8db81032ca39782d686acada44b0d9
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-09-13 15:07:11 +0200

    reviews

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,5 +1,6 @@
+"use client";
 import { notFound } from 'next/navigation';
-import '../detailedProduct.css'
+import '../detailedProduct.css';
 import Navbar from '@/components/navbar';
 
 async function fetchProduct(id) {
@@ -15,37 +16,53 @@ export default async function ProductDetailPage({ params }) {
   
   try {
     const product = await fetchProduct(id);
-
+    
+    
+    
     return (
-
       <div className="product-detail">
-        
-      <Navbar/>
-      <div className="displayBox1">
-     
-        <h1>{product.title}</h1>
-
-        <div className="product-images">
-          {product.images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`${product.title} - image ${index + 1}`}
-              className="product-image"
-            />
-          ))}
+        <Navbar />
+        <div className="displayBox1">
+          <h1>{product.title}</h1>
+          <div className="thumbnail-list">
+            {product.images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`${product.title} - thumbnail ${index + 1}`}
+                className="thumbnail-image"
+              
+              />
+            ))}
           </div>
         </div>
- <div className="displayBox2">
-        <p>{product.description}</p>
-        <p>Category: {product.category}</p>
-        <p>Brand: {product.brand}</p>
-        <p>Price: ${product.price}</p>
-        <p>In Stock: {product.stock}</p>
-        
-        <p>Tags: {product.tags.join(', ')}</p>
-        <p>Rating: {product.rating}</p>
-      </div>
+        <div className="displayBox2">
+          <p>{product.description}</p>
+          <p>Category: {product.category}</p>
+          <p>Brand: {product.brand}</p>
+          <p>Price: ${product.price}</p>
+          <p>In Stock: {product.stock}</p>
+          <p>Tags: {product.tags.join(', ')}</p>
+          <p>Rating: {product.rating}</p>
+        </div>
+
+        {/* Display Reviews */}
+        <div className='reviews'>
+          <h3>Reviews:</h3>
+          {product.reviews && product.reviews.length > 0 ? (
+            <div className="reviews">
+              {product.reviews.map((review, index) => (
+               <div key={index} className="review">
+               <p><strong>{review.author}</strong></p>
+               <p>{review.comment || 'No comment provided'}</p>
+               <p>Rating: {review.rating || 'N/A'}/5</p>
+             </div>
+              ))}
+            </div>
+          ) : (
+            <p>No reviews available for this product.</p>
+          )}
+          </div>
       </div>
     );
   } catch (error) {
